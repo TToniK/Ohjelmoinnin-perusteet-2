02e5aed60bdb5782a48c24db21aa620c94b68a25
Toni Kari <kari.toni@hotmail.com>
2018-11-12 11:12:45 +0200

    Taulukot done

---
+++ Taulukot/Taulukko1.2/Taulukko1.2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System;
+
+namespace Taulukko1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("Random keskiarvo");
+                int[] iT = new int[100];
+                Random rnd = new Random();
+                
+                for (int i = 0; i < iT.Length; i++)
+                {
+                    iT[i] = rnd.Next(50);
+                }
+                
+                int summa = iT.Sum();
+                int average = summa / 100;
+                
+                for (int i = 0; i < iT.Length; i++)
+                {
+                    Console.Write($"{i + 1}. ");
+                    Console.WriteLine(iT[i]);
+                }
+                Console.WriteLine($"keskiarvo: {average}");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Taulukot/Taulukko_1.1/Taulukko_1.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Taulukko_1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+                decimal[] productPrice = { 7.96m, 72.0m, 99.90m };
+                int[] productAmount = { 1, 3, 2 };
+                decimal[] productTotalPrice = new decimal[3];
+                productTotalPrice[0] = productPrice[0] * productAmount[0];
+                productTotalPrice[1] = productPrice[1] * productAmount[1];
+                productTotalPrice[2] = productPrice[2] * productAmount[2];
+
+
+                Console.WriteLine("Tuote 1: " + productTotalPrice[0]);
+                Console.WriteLine("Tuote 2: " + productTotalPrice[1]);
+                Console.WriteLine("Tuote 3: " + productTotalPrice[2]);
+
+
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
