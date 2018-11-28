a05faf3c907fa93e3b959d89b13780bff743a098
Toni Kari <kari.toni@hotmail.com>
2018-11-28 13:44:31 +0200

    taulukko 3 teht. tehty

---
+++ Taulukot/Taulukko_3.1/Taulukko_3.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Taulukko_3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("Ohjelma arpoo 10 lukua ja esittää ne taulukossa");
+                int[] arr = new int[10];
+                Random rnd = new Random();
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    arr[i] = rnd.Next(20);
+                }
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    Console.WriteLine($"[{j}] = {arr[j]:D2}");
+                }
+            }Console.ReadKey();
+        }
+    }
+}
